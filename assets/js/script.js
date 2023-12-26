94e20aedca4e1fb8eb657d4a6ab6a178a2b78067
lucascontee <lucascontee@icloud.com>
2023-12-26 14:23:54 -0300

    finalizando o projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,17 @@ function iniciarModal(modalID){
 }
 
 const moreButton1 = document.getElementById('moreButton1');
+const moreButton2 = document.getElementById('moreButton2');
+const moreButton3 = document.getElementById('moreButton3');
+const moreButton4 = document.getElementById('moreButton4');
+const moreButton5 = document.getElementById('moreButton5');
+const moreButton6 = document.getElementById('moreButton6');
+
 moreButton1.addEventListener('click', () => iniciarModal('modal-pokemon bulba'));
+moreButton2.addEventListener('click', () => iniciarModal('modal-pokemon ivy'));
+moreButton3.addEventListener('click', () => iniciarModal('modal-pokemon veno'));
+moreButton4.addEventListener('click', () => iniciarModal('modal-pokemon char'));
+moreButton5.addEventListener('click', () => iniciarModal('modal-pokemon meleon'));
+moreButton6.addEventListener('click', () => iniciarModal('modal-pokemon zard'));
+
+
